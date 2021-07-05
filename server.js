14de777df41d9ebb21285ba52cac0e025506918c
anbya <anbyaalibia@gmail.com>
2021-07-05 13:53:15 +0700

    anbya second change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
 
-const PORT = 3009
+const PORT = process.env.PORT || 3009
 
 app.use(cors());
 app.use(bodyParser.json());
